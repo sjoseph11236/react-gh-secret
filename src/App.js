6f8f0251450397d3e8becda3dd96a71e8ddd77da
Sayeed Joseph <sayeedjoseph@gmail.com>
2024-06-12 13:49:05 -0400

    add secret

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const getPlaylists = async () => {
+    try {
+      const youtube_api_key = process.env.REACT_APP_API_KEY;
+      const playlists_url = `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&maxResults=25&key=${youtube_api_key}&channelId=UCCj88q3bb5OnVznn7hUY_9Q`
+      const response = await fetch(playlists_url)
+      const playlists = await response.json()
+      const playlistItems = playlists.items
+      console.log('playlists', playlists.items)
+      setItems(playlistItems)
+    } catch (error) {}
+  }
+  useEffect(() => {
+    getPlaylists()
+    console.log('playlist items', items)
+  }, [items])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>The number of items { items ? items.length: '--'}</p>
     </div>
   );
 }
