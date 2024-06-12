6ec1eb0215287c83a2079203ee7986b94263964a
Sayeed Joseph <sayeedjoseph@gmail.com>
2024-06-12 14:04:48 -0400

    rmeove console.log

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-const youtube_api_key = process.env.REACT_APP_API_KEY;
 
 function App() {
+  const youtube_api_key = process.env.REACT_APP_API_KEY;
   const [items, setItems] = useState([]);
   const getPlaylists = async () => {
     try {
-      console.log('youtybe api key', youtube_api_key)
       const playlists_url = `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&maxResults=25&key=${youtube_api_key}&channelId=UCCj88q3bb5OnVznn7hUY_9Q`
       const response = await fetch(playlists_url)
       const playlists = await response.json()
