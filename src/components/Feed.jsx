3c82a9f742bd15c2fdda2fa5e819fd34fd29c9ba
Sayeed Joseph <sayeedjoseph@Sayeeds-MacBook-Pro.local>
2024-08-23 16:44:27 -0400

    remove import

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useMemo } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import { SideBar, Videos, HomeDetail, SearchFeed } from "./";
+import { SideBar, Videos, HomeDetail } from "./";
 import playlists from "../seed.json";
 import styled from "styled-components";
 
