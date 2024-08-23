f87ebaf967a690e4a7ea33a9af5cc77f4132dba7
Sayeed Joseph <sayeedjoseph@Sayeeds-MacBook-Pro.local>
2024-08-23 16:58:17 -0400

    change placeholder

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -30,7 +30,7 @@ const Feed = () => {
             <StyledInputContainer>
               <StyledInput
                 type="text"
-                placeholder="Search..."
+                placeholder="Search Playlists"
                 value={query}
                 onInput={(e) => {
                   searchPlaylist(e.target.value);
