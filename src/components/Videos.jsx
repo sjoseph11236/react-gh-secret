be9ac80b082b1000953cf2169874f01b3e8f5edd
sjoseph11236 <sayeedjoseph@gmail.com>
2025-03-13 07:54:21 -0400

    increase margin left

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -12,7 +12,7 @@ const Videos = ({ videos, direction }) => {
       justifyContent="start"
       alignItems="start"
       gap={2}
-      ml={"2px"}
+      ml={"5px"}
     >
       {videos.map((item, idx) => (
         <Box key={idx}>
