425e70457836d4e78a4a2a303f1b4765574228e5
sjoseph11236 <sayeedjoseph@gmail.com>
2025-03-27 09:17:57 -0400

    remove unused imports

--- src/components/PlaylistCard.jsx
+++ src/components/PlaylistCard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React from "react";
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 import { Link } from "react-router-dom";
